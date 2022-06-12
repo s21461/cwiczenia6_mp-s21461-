dd1bf397efb67926868b557ec184a3e587661745
s21461 <patrykkepisty@gmail.com>
2022-06-12 22:32:34 +0200

    ad

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace cwiczenia6_mp_s21461
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
